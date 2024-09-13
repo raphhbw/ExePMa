64cbff4834e0630b4c04e82cd7c57b55b51db1f1
Raphael Bendahan-West <59627474+raphhbw@users.noreply.github.com>
2024-09-13 11:21:10 +0100

    updated Database to remove iparam and include kwargs

--- PMa/Database.py
+++ PMa/Database.py
@@ -1,98 +1,78 @@
 import pandas as pd
 from astroquery.vizier import Vizier
 import astropy.units as u
-import sys
 
 class Database():
-    v_eDR3 = Vizier(columns=["**"], catalog="J/A+A/657/A7") # Kervella+22 Vizier Catalog
+    V_EDR3 = Vizier(columns=["**"], catalog="J/A+A/657/A7") # Kervella+22 Vizier Catalog
 
-    def __init__(self, discdata, gaia, iparam):
+    def __init__(self, star, **kwargs):
         
-        self.discdata = discdata
-        self.target = discdata['name']
-        self.data = self.get_data(gaia, iparam)
+        self.star = star
+        self.geometry = self.build_geometryinfo(kwargs.get("geometry", {}))
+        self.gaia = kwargs.get("gaia", 'eDR3')
 
+        self.data = self.get_data(self.gaia)
+
+    def build_geometryinfo(self, discinfo):
+        """ Build dictionary with geometry info 
+            inc and PA are given in deg.
+            Returns: Dictionary with geometry information about the system. Values depend on the geometry disctionary given to the function """
+        return {"inc":discinfo.get('inc', None), # default None if no inc is given
+                "inc_err":discinfo.get('inc_err', 1.0), # default 1.0 if no inc_err is given
+                "PA":discinfo.get('PA', None), # default None if no PA is given
+                "PA_err":discinfo.get('PA_err', 180.), # default 180. if no PA_err is given
+                }
 
     def vizier_query(self, gaia):
-        dhip = {'star':[self.target]}
-        dgaia = {'star':[self.target]}
-        dparams = {'star':[self.target]}
-        if gaia == 'eDR3':
-            results = self.v_eDR3.query_object(self.target, radius=1*u.arcmin)
-            # Get Hip data
-            dhip['S_N'] = results[0]['snrPMaH2EG3a'].data[0]    
-            dhip['PMa_ra'] = results[0]['PMaRAH2EG3a'].data[0]
-            dhip['PMa_ra_err'] = results[0]['e_PMaRAH2EG3a'].data[0]
-            dhip['PMa_dec'] = results[0]['PMaDEH2EG3a'].data[0]
-            dhip['PMa_dec_err'] = results[0]['e_PMaDEH2EG3a'].data[0]
-            # Get eDR3 data
-            dgaia['S_N'] = results[0]['snrPMaH2EG3b'].data[0]
-            dgaia['PMa_ra'] = results[0]['PMaRAH2EG3b'].data[0]
-            dgaia['PMa_ra_err'] = results[0]['e_PMaRAH2EG3b'].data[0]
-            dgaia['PMa_dec'] = results[0]['PMaDEH2EG3b'].data[0]
-            dgaia['PMa_dec_err'] = results[0]['e_PMaDEH2EG3b'].data[0]
+        """ Query Vizier catalog for Kervella PMa information. 
+            Get info from both gaia and hipparcos. 
+            Returns: info for hip, info for gaia, info on star """
+        
+        # Initialise the dictionaries
+        dhip = {'star':[self.star]}
+        dgaia = {'star':[self.star]}
+        dparams = {'star':[self.star]}
 
+        if gaia == 'eDR3':
+            # Query catalog
+            results = self.V_EDR3.query_object(self.star, radius=1*u.arcmin)
+            # Get Hip data
+            dhip['S_N'] = results[0]['snrPMaH2EG3a'].data[0] # SNR hip - HipeDR3
+            dhip['PMa_ra'] = results[0]['PMaRAH2EG3a'].data[0] # PMa Right Ascension hip - HipeDR3
+            dhip['PMa_ra_err'] = results[0]['e_PMaRAH2EG3a'].data[0] # PMa Right Ascension err hip - HipeDR3
+            dhip['PMa_dec'] = results[0]['PMaDEH2EG3a'].data[0] # PMa Dec hip - HipeDR3
+            dhip['PMa_dec_err'] = results[0]['e_PMaDEH2EG3a'].data[0] # PMa Dec err hip - HipeDR3
+            
+            # Get eDR3 data
+            dgaia['S_N'] = results[0]['snrPMaH2EG3b'].data[0] # SNR eDR3 - HipeDR3
+            dgaia['PMa_ra'] = results[0]['PMaRAH2EG3b'].data[0] # PMa Right Ascension eDR3 - HipeDR3
+            dgaia['PMa_ra_err'] = results[0]['e_PMaRAH2EG3b'].data[0] # PMa Right Ascension err eDR3 - HipeDR3
+            dgaia['PMa_dec'] = results[0]['PMaDEH2EG3b'].data[0] # PMa Dec eDR3 - HipeDR3
+            dgaia['PMa_dec_err'] = results[0]['e_PMaDEH2EG3b'].data[0] # PMa Dec err eDR3 - HipeDR3
+
+            # Get star info
             dparams['parallax'] = results[0]['PlxG3'].data[0] # mas
             dparams['dpc'] = 1./(dparams['parallax']*1.0e-3)
-            
             dparams['mstar'] = results[0]['M1'].data[0]
 
-        elif gaia == 'DR2':
-            # TODO update with Kervella+19 (if needed)
-            print('Need to update')
-            sys.exit()
-            # # Get Hip data
-            # d['S_N_hip'] = results[0]['snrPMaH2G2'].data[0]    
-            # d['PMa_hip_ra'] = results[0]['PMaRAH2G2'].data[0]
-            # d['PMa_hip_ra_err'] = results[0]['e_PMaRAH2G2'].data[0]
-            # d['PMa_hip_dec'] = results[0]['PMaDEH2EG3a'].data[0]
-            # d['PMa_hip_dec_err'] = results[0]['e_PMaDEH2EG3a'].data[0]
-            # # Get eDR3 data
-            # d['S_N_dr3'] = results[0]['snrPMaH2EG3b'].data[0]
-            # d['PMa_dr3_ra'] = results[0]['PMaRAH2EG3b'].data[0]
-            # d['PMa_dr3_ra_err'] = results[0]['e_PMaRAH2EG3b'].data[0]
-            # d['PMa_dr3_dec'] = results[0]['PMaDEH2EG3b'].data[0]
-            # d['PMa_dr3_dec_err'] = results[0]['e_PMaDEH2EG3b'].data[0]
+            # Add geometry info to star info
+            dparams['PA'] = self.geometry['PA']
+            dparams['PA_err'] = self.geometry['PA_err']
+            dparams['inc'] = self.geometry['inc']
+            dparams['inc_err'] = self.geometry['inc_err']
 
+        elif gaia == 'DR2':
+            return NotImplementedError('DR2 epoch still needs to be implemented from Kervella+2019')
         else:
-            print('wrong gaia epoch')
-            sys.exit()
+            return NotImplementedError('Wrong choice of gaia epoch.')
 
         return dhip, dgaia, dparams
     
-    def read_param(self, d, iparam):
-        if iparam:
-            d['PA'] = self.discdata['PA']['median'] # deg
-            d['PA_err'] = self.discdata['PA']['sigma'] # deg
-
-            if 'l' in self.discdata['inc'].keys():
-                # lower limit
-                print('inc lower limit of ', self.discdata['inc']['l'])
-                d['inc'] = (self.discdata['inc']['l']+90.)/2.
-                d['inc_err'] = (90.-self.discdata['inc']['l'])/3.
-                
-            elif 'u' in self.discdata['inc'].keys():
-                # upper limit
-                print('inc upper limit of ', self.discdata['inc']['u']) 
-                d['inc'] = self.discdata['inc']['u']/2.
-                d['inc_err'] = d['inc']/3.
-                
-            else: 
-                d['inc'] = self.discdata['inc']['median'] # deg
-                d['inc_err'] = self.discdata['inc']['sigma'] # deg
-        else:
-            d['PA'] = self.discdata['PA']
-            d['PA_err'] = self.discdata['PA_err']
-            d['inc'] = self.discdata['inc']
-            d['inc_err'] = self.discdata['inc_err']
-
-        return d
-    
-    def get_data(self, gaia, iparam):
+    def get_data(self, gaia):
         d= {}
-        # dparams = {'star':[self.target]}
-        dhip, dgaia, dparams = self.vizier_query(gaia)  
-        dparams = self.read_param(dparams, iparam)
+        
+        # Query data from Kervella vizier catalog
+        dhip, dgaia, dparams = self.vizier_query(gaia)
 
         d['Hipparcos'] = pd.DataFrame(data=dhip).set_index('star')
         d[gaia] = pd.DataFrame(data=dgaia).set_index('star')
